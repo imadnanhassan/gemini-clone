50a4742ee7fa0b3ada1c56c46a5272cc59755d8a
imadnanhassan <abulhassn.dev@gmail.com>
2024-04-06 04:46:08 +0600

    sidebar design ok

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./sidebar.css";
+import { assets } from "../../assets/assets";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          <p>New Chat</p>
+        </div>
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is React ...</p>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="buttom-items recent-entry">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+        <div className="buttom-items recent-entry">
+          <img src={assets.history_icon} alt="" />
+          <p>Activity</p>
+        </div>
+        <div className="buttom-items recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          <p>Settings</p>
+        </div>
+      </div>
+    </div>
+  );
+}
